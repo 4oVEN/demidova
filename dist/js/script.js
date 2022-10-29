f13da292256258e3d64db6b7d525553cf2f15807
Nikita <ndemidovn@gmail.com>
2022-10-29 22:23:40 +0300

    fixed accordions

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const accordions = document.querySelectorAll('.services__item');
+  const accordions = document.querySelectorAll('.services__item-inner');
 
   accordions.forEach(el => {
     el.addEventListener('click', (e) => {
-      const self = e.currentTarget;
+      // const self = e.currentTarget;
       // const control = self.querySelector('.services__subtitle');
       // const content = self.querySelector('.services__items');
 
-      self.classList.toggle('open');
+      // self.classList.toggle('open');
+
+      console.log(el)
+
+      el.parentNode.classList.toggle('open')
+
+      console.log(el.parentNode.classList.contains('open'))
+
+      // if (el.parentNode.classList.contains('open')) {
+      //   el.closest('.services__wrapper').classList.add('parent')
+
+      // } else {
+      //   el.closest('.services__wrapper').classList.remove('parent')
+      // }
     });
   });
 
@@ -16,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('form');
   form.addEventListener('submit', formSend);
 
-  async function formSend(e){
+  async function formSend(e) {
     e.preventDefault();
 
     let formData = new FormData(form);
@@ -24,9 +37,9 @@ document.addEventListener('DOMContentLoaded', () => {
       method: 'POST',
       body: formData
     });
-    if (response.ok){
+    if (response.ok) {
       form.reset();
-    }else {
+    } else {
       alert('Ошибка!');
     }
   }
